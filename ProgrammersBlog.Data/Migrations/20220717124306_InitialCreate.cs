56d563350b2bdb42e6f1c94098fe062135371272
ahmet çiftçi <ciftciahmet70@hotmail.com>
2022-07-21 17:08:42 +0300

    Add project files.

---
+++ ProgrammersBlog.Data/Migrations/20220717124306_InitialCreate.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProgrammersBlog.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedByName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ModifiedByName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedByName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ModifiedByName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "VARBINARY(500)", nullable: false),
+                    Username = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    Picture = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedByName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ModifiedByName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Content = table.Column<string>(type: "NVARCHAR(MAX)", nullable: false),
+                    Thumbnail = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ViewsCount = table.Column<int>(type: "int", nullable: false),
+                    CommentCount = table.Column<int>(type: "int", nullable: false),
+                    SeoAuthor = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    SeoDescription = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
+                    SeoTags = table.Column<string>(type: "nvarchar(70)", maxLength: 70, nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedByName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ModifiedByName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Articles_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Articles_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: false),
+                    ArticleId = table.Column<int>(type: "int", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    CreatedByName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    ModifiedByName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Note = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Articles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "CreatedByName", "CreatedDate", "Description", "IsActive", "IsDeleted", "ModifiedByName", "ModifiedDate", "Name", "Note" },
+                values: new object[,]
+                {
+                    { 1, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 633, DateTimeKind.Local).AddTicks(3919), "C# programlama dili ile ilgili en güncel bilgiler", true, false, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 633, DateTimeKind.Local).AddTicks(3933), "C#", "C# blog kategorisi" },
+                    { 2, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 633, DateTimeKind.Local).AddTicks(3943), "C++ programlama dili ile ilgili en güncel bilgiler", true, false, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 633, DateTimeKind.Local).AddTicks(3944), "C++", "C++ blog kategorisi" },
+                    { 3, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 633, DateTimeKind.Local).AddTicks(3948), "JavaScript programlama dili ile ilgili en güncel bilgiler", true, false, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 633, DateTimeKind.Local).AddTicks(3949), "JavaScript", "JavaScript blog kategorisi" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "Id", "CreatedByName", "CreatedDate", "Description", "IsActive", "IsDeleted", "ModifiedByName", "ModifiedDate", "Name", "Note" },
+                values: new object[] { 1, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 637, DateTimeKind.Local).AddTicks(2173), "Admin Rolü, tüm haklara sahiptir.", true, false, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 637, DateTimeKind.Local).AddTicks(2186), "Admin", "Admin Rolüdür" });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "CreatedByName", "CreatedDate", "Description", "Email", "FirstName", "IsActive", "IsDeleted", "LastName", "ModifiedByName", "ModifiedDate", "Note", "PasswordHash", "Picture", "RoleId", "Username" },
+                values: new object[] { 1, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 647, DateTimeKind.Local).AddTicks(5968), "İlk Admin Kullanıcı", "aciftci976@gmail.com", "Ahmet", true, false, "Çiftçi", "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 647, DateTimeKind.Local).AddTicks(5984), "Admin Kullanıcısı", new byte[] { 48, 49, 57, 50, 48, 50, 51, 97, 55, 98, 98, 100, 55, 51, 50, 53, 48, 53, 49, 54, 102, 48, 54, 57, 100, 102, 49, 56, 98, 53, 48, 48 }, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSX4wVGjMQ37PaO4PdUVEAliSLi8-c2gJ1zvQ&usqp=CAU", 1, "ahmetciftci" });
+
+            migrationBuilder.InsertData(
+                table: "Articles",
+                columns: new[] { "Id", "CategoryId", "CommentCount", "Content", "CreatedByName", "CreatedDate", "Date", "IsActive", "IsDeleted", "ModifiedByName", "ModifiedDate", "Note", "SeoAuthor", "SeoDescription", "SeoTags", "Thumbnail", "Title", "UserId", "ViewsCount" },
+                values: new object[] { 1, 1, 1, "Lorem Ipsum, kısaca Lipsum, masaüstü yayıncılık ve basın yayın sektöründe kullanılan taklit yazı bloğu olarak tanımlanır. Lipsum, oluşturulacak şablon ve taslaklarda içerik yerine geçerek yazı bloğunu doldurmak için kullanılır.", "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 629, DateTimeKind.Local).AddTicks(8544), new DateTime(2022, 7, 17, 15, 43, 5, 629, DateTimeKind.Local).AddTicks(7708), true, false, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 629, DateTimeKind.Local).AddTicks(8977), "C# 9.0 ve .NET 5 yenilikleri", "Ahmet Çiftçi", "C# 9.0 ve .NET 5 yenilikleri", "C#, C# 9, .NET 5, NET Framework, .NET Core", "Default.jpg", "C# 9.0 ve .NET 5 yenilikleri", 1, 100 });
+
+            migrationBuilder.InsertData(
+                table: "Articles",
+                columns: new[] { "Id", "CategoryId", "CommentCount", "Content", "CreatedByName", "CreatedDate", "Date", "IsActive", "IsDeleted", "ModifiedByName", "ModifiedDate", "Note", "SeoAuthor", "SeoDescription", "SeoTags", "Thumbnail", "Title", "UserId", "ViewsCount" },
+                values: new object[] { 2, 2, 1, "Lorem Ipsum, 500 yıl boyunca varlığını sürdürmekle kalmamış ve günümüzde elektronik yazı tipinin gerektiği birçok konuda hazır bir araç olarak kullanılmaya başlanmıştır. Lipsum 1960'larda içinde Lorem Ipsum paragraflarının bulunduğu letrasetlerin piyasaya çıkması ve 1990'larda Lorem Ipsum versiyonlarını içeren Aldus Pagemaker gibi programlarla beraber yaygın hale gelmiştir.", "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 629, DateTimeKind.Local).AddTicks(9955), new DateTime(2022, 7, 17, 15, 43, 5, 629, DateTimeKind.Local).AddTicks(9954), true, false, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 629, DateTimeKind.Local).AddTicks(9956), "C++ 11 ve 19 yenilikleri", "Ahmet Çiftçi", "C++ 11 ve 19 yenilikleri", "C++ 11 ve 19 yenilikleri", "Default.jpg", "C++ 11 ve 19 yenilikleri", 1, 295 });
+
+            migrationBuilder.InsertData(
+                table: "Articles",
+                columns: new[] { "Id", "CategoryId", "CommentCount", "Content", "CreatedByName", "CreatedDate", "Date", "IsActive", "IsDeleted", "ModifiedByName", "ModifiedDate", "Note", "SeoAuthor", "SeoDescription", "SeoTags", "Thumbnail", "Title", "UserId", "ViewsCount" },
+                values: new object[] { 3, 3, 1, "Lorem Ipsum, 500 yıl boyunca varlığını sürdürmekle kalmamış ve günümüzde elektronik yazı tipinin gerektiği birçok konuda hazır bir araç olarak kullanılmaya başlanmıştır. Lipsum 1960'larda içinde Lorem Ipsum paragraflarının bulunduğu letrasetlerin piyasaya çıkması ve 1990'larda Lorem Ipsum versiyonlarını içeren Aldus Pagemaker gibi programlarla beraber yaygın hale gelmiştir.", "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 629, DateTimeKind.Local).AddTicks(9992), new DateTime(2022, 7, 17, 15, 43, 5, 629, DateTimeKind.Local).AddTicks(9991), true, false, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 629, DateTimeKind.Local).AddTicks(9993), "JavaScript ES2019 ve ES2020 yenilikleri", "Ahmet Çiftçi", "JavaScript ES2019 ve ES2020 yenilikleri", "JavaScript ES2019 ve ES2020 yenilikleri", "Default.jpg", "JavaScript ES2019 ve ES2020 yenilikleri", 1, 12 });
+
+            migrationBuilder.InsertData(
+                table: "Comments",
+                columns: new[] { "Id", "ArticleId", "CreatedByName", "CreatedDate", "IsActive", "IsDeleted", "ModifiedByName", "ModifiedDate", "Note", "Text" },
+                values: new object[] { 1, 1, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 635, DateTimeKind.Local).AddTicks(7547), true, false, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 635, DateTimeKind.Local).AddTicks(7561), "C# makale yorumu", "Güzel bir yazı olmuş." });
+
+            migrationBuilder.InsertData(
+                table: "Comments",
+                columns: new[] { "Id", "ArticleId", "CreatedByName", "CreatedDate", "IsActive", "IsDeleted", "ModifiedByName", "ModifiedDate", "Note", "Text" },
+                values: new object[] { 2, 2, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 635, DateTimeKind.Local).AddTicks(7572), true, false, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 635, DateTimeKind.Local).AddTicks(7573), "C++ makale yorumu", "Güzel bir yazı olmuş." });
+
+            migrationBuilder.InsertData(
+                table: "Comments",
+                columns: new[] { "Id", "ArticleId", "CreatedByName", "CreatedDate", "IsActive", "IsDeleted", "ModifiedByName", "ModifiedDate", "Note", "Text" },
+                values: new object[] { 3, 3, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 635, DateTimeKind.Local).AddTicks(7576), true, false, "InitialCreate", new DateTime(2022, 7, 17, 15, 43, 5, 635, DateTimeKind.Local).AddTicks(7577), "JavaScript makale yorumu", "Güzel bir yazı olmuş." });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_CategoryId",
+                table: "Articles",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_UserId",
+                table: "Articles",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ArticleId",
+                table: "Comments",
+                column: "ArticleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
